1126b05600c61b9cb2c05bf0cc6670656dfe4793
Ana <angy.aa@gmail.com>
2019-12-17 17:38:01 +0100

    Implement landing page and display data

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../customer.service';
+import { IOffer } from '../types/offer.interface';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-  constructor() { }
+  public offers: IOffer[] = [];
+
+  constructor(
+    private customerService: CustomerService,
+  ) { }
 
   ngOnInit() {
+    this.loadCustomerOffers();
+  }
+
+  private loadCustomerOffers() {
+    this.customerService.getOffers().subscribe(
+      (response) => {
+        console.log('response :', response);
+        this.offers = response;
+      },
+      (error) => {
+        console.log(error);
+        alert('Something went wrong');
+      }
+    );
   }
 
 }
