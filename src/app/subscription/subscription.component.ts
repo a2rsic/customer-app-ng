aa5f57a76550bfe543a0122550bfbccbb65eb5b2
Ana <angy.aa@gmail.com>
2019-12-18 18:44:54 +0100

    Implement subscription page

--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../customer.service';
+import { ISubscription } from '../types/subscription.interface';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-subscription',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscriptionComponent implements OnInit {
 
-  constructor() { }
+  public subscriptions: ISubscription[] = [];
+  public isLoading = true;
+
+  constructor(
+    private customerService: CustomerService,
+    private route: ActivatedRoute,
+    private loaction: Location,
+  ) { }
 
   ngOnInit() {
+    this.loadSubscription();
+  }
+
+  public goBack() {
+    this.loaction.back();
+  }
+
+  private loadSubscription() {
+    const offerId = +this.route.snapshot.paramMap.get('id');
+    this.customerService.getSubscription(offerId).subscribe(
+      (subscriptions) => {
+        this.isLoading = false;
+        this.subscriptions = subscriptions;
+      },
+      (error) => {
+        this.isLoading = false;
+        console.log(error);
+        alert('Something went wrong');
+      }
+    );
   }
 
 }
